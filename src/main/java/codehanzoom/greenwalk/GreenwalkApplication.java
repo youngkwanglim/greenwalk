3c18b631e07fb7238b1309cacbf161a833e4d047
Youngkwang Lim <richard08@naver.com>
2024-03-21 21:53:05 +0900

    초기설정 완료

---
+++ src/main/java/codehanzoom/greenwalk/GreenwalkApplication.java
@@ -0,0 +1,13 @@
+package codehanzoom.greenwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenwalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenwalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codehanzoom/greenwalk/GreenwalkApplicationTests.java
@@ -0,0 +1,13 @@
+package codehanzoom.greenwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenwalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
