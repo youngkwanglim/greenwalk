1746f14daa36bb51566e55d32bbf713f9f2d6cc3
Youngkwang Lim <richard08@naver.com>
2024-05-08 10:25:31 +0900

    쓰레기 사진 분석 후 반환 dto 추가

--- src/main/java/codehanzoom/greenwalk/plogging/dto/PloggingResponse.java
+++ src/main/java/codehanzoom/greenwalk/plogging/dto/PloggingResponse.java
@@ -7,9 +7,18 @@ public class PloggingResponse {
 
     private int trashCount;
     private String imageUrl;
+    private int point;
 
     public PloggingResponse(int trashCount, String imageUrl) {
         this.trashCount = trashCount;
         this.imageUrl = imageUrl;
     }
+
+    public PloggingResponse(int trashCount, String imageUrl, int point) {
+        this.trashCount = trashCount;
+        this.imageUrl = imageUrl;
+        this.point = point;
+    }
+
+
 }
